4d70a0beba28690892b46beedd3fd76b3538752f
Rajan Jha <rajanjharb@gmail.com>
2025-06-26 00:30:31 +0545

    update: improve index.js content/design

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "../container/container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Login from "./Login";
@@ -11,6 +10,7 @@ import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Button from "./Button";
 import Hero from "./Hero/Hero";
+import Container from "../container/Container";
 
 export {
   Header,
